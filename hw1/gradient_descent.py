4c898a8d42d852021fc5aa39d9286a32bf6fe883
Yist Lin <yishen992@gmail.com>
2017-03-22 23:14:09 +0800

    remove square terms of WIND_DIRECTION, and got 5.294 on public set

--- hw1/gradient_descent.py
+++ hw1/gradient_descent.py
@@ -19,6 +19,7 @@ train_data = np.concatenate((train_data[:6], train_data[7:]))
 # print('train_data.shape =', train_data.shape)
 
 needed_cols = [2, 7, 8, 9, 10, 12, 14, 15, 16, 17]
+square_cols = [2, 7, 8, 9, 10, 12, 16, 17]
 target_col = 9 # PM2.5
 num_col = len(needed_cols)
 prev_col = 9
@@ -29,14 +30,21 @@ for mon in range(len(train_data)):
         X.append(np.array([train_data[mon][col][i:i+prev_col] for col in needed_cols]))
         Y.append(train_data[mon][target_col][i+prev_col])
 
+sX = []
+for mon in range(len(train_data)):
+    for i in range(480 - prev_col):
+        sX.append(np.array([train_data[mon][col][i:i+prev_col] for col in square_cols]))
+
 # Training
 X = np.array(X)
+sX = np.array(sX)
 col_o3 = X[:, 1].reshape(len(X), prev_col)
 col_pm2 = X[:, 3].reshape(len(X), prev_col)
 col_mul = col_o3 * col_pm2
 X = X.reshape(len(X), prev_col * num_col)
+sX = sX.reshape(len(sX), prev_col * len(square_cols))
 Y = np.array(Y)
-X = np.concatenate((X, X**2, col_mul), axis=1)
+X = np.concatenate((X, sX**2, col_mul), axis=1)
 
 # Normalization
 feat_max = np.max(X, axis=0)
@@ -47,7 +55,7 @@ train_X = X
 train_Y = Y
 
 # Declare basic settings
-iteration = 35000
+iteration = 40000
 lr = 0.5
 b_lr = 1e-20
 w_lr = np.full((X.shape[1]), 1e-20)
@@ -55,7 +63,7 @@ b = 0.0
 w = np.ones(shape=(X.shape[1]))
 prev_rmse = 1e20
 rising_cnt = 0
-reg_lambda = 1e-2
+reg_lambda = 1e-3
 
 for i in range(iteration):
     b_grad = 0.0
@@ -108,17 +116,22 @@ test_X = []
 test_Y = []
 for i in range(test_data.shape[0]):
     test_X.append(np.array([test_data[i][col][9-prev_col:] for col in needed_cols]))
+sX = []
+for i in range(test_data.shape[0]):
+    sX.append(np.array([test_data[i][col][9-prev_col:] for col in square_cols]))
 
 test_X = np.array(test_X)
+sX = np.array(sX)
 
 # Testing
-for x in test_X:
-    col_o3 = x[1].flatten()
-    col_pm2 = x[3].flatten()
+for i in range(len(test_X)):
+    col_o3 = test_X[i][1].flatten()
+    col_pm2 = test_X[i][3].flatten()
     col_mul = col_o3 * col_pm2
-    x_flat = x.flatten()
+    x_flat = test_X[i].flatten()
+    sx_flat = sX[i].flatten()
 
-    x_flat = np.concatenate((x_flat, x_flat ** 2, col_mul))
+    x_flat = np.concatenate((x_flat, sx_flat ** 2, col_mul))
     x_flat = (x_flat - feat_min) / (feat_max - feat_min + 1e-20)
     test_Y.append(x_flat.dot(w) + b)
 
