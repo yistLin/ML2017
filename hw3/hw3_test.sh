07ee5768f52ee6cb62cf11884c2182c65a33e182
Yist Lin <yishen992@gmail.com>
2017-04-13 20:16:44 +0800

    separate training and prediction

---
+++ hw3/hw3_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# python version: python3.5
+python3 predict.py $1 $2
---
+++ hw3/predict.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+import sys
+import numpy as np
+from keras.models import load_model
+
+def read_features(filename):
+    col0 = []
+    col1 = []
+    with open(filename, 'r') as f:
+        next(f)
+        for line in f:
+            fields = line.strip().split(',')
+            col0.append(int(fields[0]))
+            col1.append(list(map(int, fields[1].split())))
+
+    return np.array(col0), np.array(col1)
+
+def predict(model, test_features):
+    X = test_features / 255
+    X.shape = (-1, 48, 48, 1)
+    return model.predict_classes(X)
+
+def main():
+    model = load_model('model.hdf5')
+    ids, test_features = read_features(sys.argv[1])
+    results = predict(model, test_features)
+
+    with open(sys.argv[2], 'w') as output_file:
+        outputs = ['id,label']
+        for i, label in enumerate(results):
+            outputs.append('%d,%d' % (i, label))
+        outputs.append('')
+        output_file.write('\n'.join(outputs))
+
+if __name__ == '__main__':
+    main()
