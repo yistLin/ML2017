c7addb6432db796dcd89c334bca068a9303b377c
yist <yist@hao-login.localdomain>
2017-05-12 02:01:02 +0800

    first commit of hw5

---
+++ hw5/rnn.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+import argparse
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.callbacks import ModelCheckpoint, CSVLogger
+
+if __name__ == '__main__':
+    print('kerker')
+
