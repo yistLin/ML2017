50a290518e6cd183d2bcc313237383a30b411bcf
b03902048 <b03902048@csie.ntu.edu.tw>
2017-05-11 22:48:43 +0800

    only use mean distance of std to predict dim

--- hw4/dim.py
+++ hw4/dim.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python3
-import os
 import argparse
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
 
 class Dim:
     def __init__(self, data_path):
@@ -13,38 +11,12 @@ class Dim:
         print('raw_data loaded from {}'.format(data_path))
 
     def load_stds(self):
-        data_path = 'stds.npy'
-        if os.path.exists(data_path):
-            self.stds = np.load(data_path)
-            print('stds loaded from {}'.format(data_path))
-        else:
-            from getdata import calc_std
-            self.stds = calc_std(return_std=True)
-            print('stds generated from getdata.py')
+        import getdata
+        self.stds = getdata.calc_std(return_std=True)
+        print('stds generated from getdata.py')
 
     def calc_std(self):
-        self.predictions = np.empty(200)
-        dists = np.empty(60)
-
-        for i in range(200):
-            std = np.std(self.raw_data[str(i)])
-            print('dataset {}, std = {}'.format(i, std))
-
-            for int_dim in range(60):
-                dists[int_dim] = np.mean(np.abs(self.stds[int_dim] - std))
-                # print('  ==> dists[{}] = {}'.format(int_dim, dists[int_dim]))
-
-            idx_min_dist = np.argmin(dists)
-            self.predictions[i] = idx_min_dist + 1
-
-            print('  ==> idx_min_dist = {}\n'.format(idx_min_dist))
-
-    def calc_nn(self):
-        import getdata
-        dists = getdata.calc_nn(return_data=True)
-        print('distances generated from getdata.py')
-        return
-
+        self.load_stds()
         self.predictions = np.empty(200)
         dists = np.empty(60)
 
@@ -73,13 +45,12 @@ class Dim:
             output_file.write('\n')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Dimensionality Reduction with PCA.')
+    parser = argparse.ArgumentParser(description='Intrinsic dimension.')
     parser.add_argument('data_path', help='data path')
     parser.add_argument('output_path', help='output file path')
     args = parser.parse_args()
 
     dim = Dim(args.data_path)
-    # dim.load_stds()
-    # dim.calc_std()
-    dim.calc_nn()
-    # dim.write_output(args.output_path)
+    dim.calc_std()
+    dim.write_output(args.output_path)
+
