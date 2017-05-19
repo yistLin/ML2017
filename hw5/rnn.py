0c21cbe90cf322e7a1c54bd42b896e6e0d05f7cc
yistLin <yishen992@gmail.com>
2017-05-19 22:02:53 +0800

    apply ta sample code

--- hw5/rnn.py
+++ hw5/rnn.py
@@ -1,202 +1,144 @@
 #!/usr/local/bin/python3
 import pickle
 import numpy as np
-import keras.backend as K
 from utils import DataReader
+from utils import f1_score
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation
-from keras.layers import LSTM, SimpleRNN, Embedding, TimeDistributed, RepeatVector, Bidirectional
-from keras.callbacks import ModelCheckpoint, CSVLogger
-
-
-def precision(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-
-def recall(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-
-def f1_score(y_true, y_pred):
-    """Compute the F1 score
-    
-    Parameters:
-        y_true: pass by fit function
-        y_pred: pass by fit function
-    """
-
-    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
-        return 0
-
-    p = precision(y_true, y_pred)
-    r = recall(y_true, y_pred)
-    bb = 1
-    f_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
-    return f_score
+from keras.layers import LSTM, SimpleRNN, GRU, Embedding, Bidirectional
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class ArtikelKlassfizier:
-    def __init__(self, max_seq_len=500):
-        self.max_seq_len = max_seq_len
+    def __init__(self):
+        self.tokenizer = None
+        self.binarizer = None
+        self.embedding_dim = 200
 
-    def fit(self, tags, texts):
-        """Fit the training data
+    def get_embedding_dict(self, path):
+        embedding_dict = {}
+        with open(path, 'r') as f:
+            for line in f:
+                values = line.split(' ')
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                embedding_dict[word] = coefs
+        return embedding_dict
 
-        Parameters:
-            data <numpy.array>
-                the data for model training
-        """
+    def fit(self, tags, sentences, wordvec_path):
+        self.tokenizer = Tokenizer()
+        self.tokenizer.fit_on_texts(sentences)
+        self.binarizer = MultiLabelBinarizer().fit(tags)
 
-        # calculate number of words
-        word2id = {'_PAD': 0}
-        word_cnt = len(word2id)
-        for s in texts:
-            for word in s:
-                if word not in word2id:
-                    word2id[word] = word_cnt
-                    word_cnt += 1
-        nb_words = len(word2id)
-        print('nb_words =', nb_words)
+        self.word_index = self.tokenizer.word_index
+        self.tag_classes = self.binarizer.classes_
+        self.nb_words = len(self.word_index)
+        self.nb_tags = len(self.tag_classes)
+        print('# of word_index =', self.nb_words)
+        print('# of tags =', self.nb_tags)
 
-        # transfer to ids
-        id_arr = []
-        for s in texts:
-            ids = [word2id[word] for word in s]
-            id_arr.append(ids)
+        embedding_dict = self.get_embedding_dict(wordvec_path)
+        self.embedding_matrix = np.zeros(shape=(self.nb_words+1, self.embedding_dim))
+        for word, i in self.word_index.items():
+            embedding_vector = embedding_dict.get(word, np.zeros(shape=(self.embedding_dim)))
+            self.embedding_matrix[i] = embedding_vector
 
-        # pad to specific length
-        X_train = []
-        for ids in id_arr:
-            pad_ids = [0] * (self.max_seq_len - len(ids)) + ids
-            X_train.append(pad_ids)
-        X_train = np.array(X_train)
+    def train(self, X, Y, model_path):
+        X = self.tokenizer.texts_to_sequences(X)
+        self.Y = self.binarizer.transform(Y)
+        self.X = pad_sequences(X)
+        self.max_seq_len = self.X.shape[1]
 
-        # calculate number of tags
-        tag2id = {}
-        id2tag = []
-        tag_cnt = 0
-        for t in tags:
-            for tag in t:
-                if tag not in tag2id:
-                    tag2id[tag] = tag_cnt
-                    id2tag.append(tag)
-                    tag_cnt += 1
-        nb_tags = len(tag2id)
-        print('nb_tags =', nb_tags)
+        print('# of training data =', self.X.shape[0])
+        print('max_seq_len =', self.max_seq_len)
 
-        # convert tags to tag_ids
-        Y_train = np.zeros(shape=(len(X_train), nb_tags))
-        for i, t in enumerate(tags):
-            Y_train[i, [tag2id[tag] for tag in t]] = 1
+        model = Sequential()
+        model.add(Embedding(
+                self.nb_words + 1,
+                self.embedding_dim,
+                weights=[self.embedding_matrix],
+                input_length=self.max_seq_len,
+                trainable=False))
+        model.add(LSTM(128, activation='tanh', dropout=0.2))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(self.nb_tags, activation='sigmoid'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=[f1_score])
 
-        self.nb_words = nb_words
-        self.nb_tags = nb_tags
-        self.word2id = word2id
-        self.tag2id = tag2id
-        self.id2tag = id2tag
-        self.X_train = X_train
-        self.Y_train = Y_train
+        model.summary()
+        model.fit(self.X, self.Y, epochs=50, batch_size=32, validation_split=0.1)
 
-    def build_model(self, embedding_size=32):
-        """Build the model
+        model.save(model_path)
 
-        Parameters:
-            embedding_size <int>
-                the embedding size of word embedding
-        """
+    def predict(self, tokens_list, output_path):
+        with open('rnn-labeler.pkl', 'rb') as f:
+            labeler = pickle.load(f)
+        with open('rnn-binarizer.pkl', 'rb') as f:
+            binarizer = pickle.load(f)
 
-        self.model = Sequential()
-        self.model.add(Embedding(self.nb_words, embedding_size, input_length=self.max_seq_len))
-        # self.model.add(LSTM(64, return_sequences=True))
-        # self.model.add(LSTM(64))
-        # self.model.add(Dense(64, activation='relu'))
-        # self.model.add(RepeatVector(38))
-        # self.model.add(LSTM(64, return_sequences=True))
-        # self.model.add(TimeDistributed(Dense(1, activation='sigmoid')))
-        # self.model.compile(loss='mse', optimizer='adam', metrics=[f1_score])
-        
-        # self.model.add(LSTM(64, return_sequenes=True))
-        self.model.add(LSTM(64, return_sequences=True))
-        self.model.add(Dropout(0.4))
-        self.model.add(SimpleRNN(64))
-        self.model.add(Dense(self.nb_tags, activation='sigmoid'))
-        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[f1_score])
+        for i, tokens in enumerate(tokens_list):
+            if len(tokens) < self.max_seq_len:
+                tokens_list[i] += ['_PAD'] * (self.max_seq_len - len(tokens))
+            elif len(tokens) > self.max_seq_len:
+                tokens_list[i] = tokens[:self.max_seq_len]
 
-        print(self.model.summary())
-
-    def train(self):
-        print('train with training data.')
-        self.model.fit(self.X_train, self.Y_train, epochs=30, batch_size=32, validation_split=0.1)
-
-    def dump(self):
-        with open('word2id.pkl', 'wb') as f:
-            pickle.dump(self.word2id, f)
-
-        with open('id2tag.pkl', 'wb') as f:
-            pickle.dump(self.id2tag, f)
-
-        self.model.save('model.hdf5')
-
-    def predict(self, data, output_path):
-        with open('word2id.pkl', 'rb') as f:
-            word2id = pickle.load(f)
-        with open('id2tag.pkl', 'rb') as f:
-            id2tag = pickle.load(f)
-        
-        X_test = []
-        for words in data:
-            ids = [word2id.get(word, 0) for word in words]
-            if len(ids) < self.max_seq_len:
-                ids = [0] * (self.max_seq_len - len(ids)) + ids
-            elif len(ids) > self.max_seq_len:
-                ids = ids[:self.max_seq_len]
-            X_test.append(ids)
-    
-        X_test = np.array(X_test)
+        X_test = labeler.transform(tokens_list)
         predictions = self.model.predict(X_test)
         print('predictions.shape =', predictions.shape)
-    
-        results = []
-        for dist in predictions:
-            tags = [id2tag[id[0]] for id in np.argwhere(dist > 0.5)]
-            results.append(tags)
-    
+
+        results = binarizer.inverse_transform(np.round(predictions))
+
         with open(output_path, 'w') as out_f:
-            outputs = ['id,tags\n']
+            outputs = ['"id","tags"\n']
             for idx, tags in enumerate(results):
-                outputs.append('{},"{}"\n'.format(idx, ','.join(tags)))
+                outputs.append('"{}","{}"\n'.format(idx, ' '.join(tags)))
             out_f.write(''.join(outputs))
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='RNN tag predictor.')
-    parser.add_argument('data_path', help='images path')
-    parser.add_argument('--predict', help='predict from saved model')
+    parser.add_argument('--train_data', help='data path')
+    parser.add_argument('--test_data', help='test data path')
+    parser.add_argument('--wordvec', help='glove word2vec path')
+    parser.add_argument('--class_path', default='rnn-classifier.pkl')
+    parser.add_argument('--model_path', default='rnn-model.hdf5')
+    parser.add_argument('--fit', action='store_true')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--predict', action='store_true')
     args = parser.parse_args()
 
     print('Running rnn.py as main program.')
 
     reader = DataReader()
+    model_path = args.model_path
+    class_path = args.class_path
 
-    if not args.predict:
-        _, tags, texts = reader.read_data(args.data_path)
-        print(texts)
-        exit()
-        ak = ArtikelKlassfizier(max_seq_len=400)
-        ak.fit(tags, texts)
-        ak.build_model(embedding_size=128)
-        ak.train()
-        ak.dump()
-    else:
-        _, texts = reader.read_test_data(args.data_path)
-        ak = ArtikelKlassfizier(max_seq_len=400)
-        ak.model = load_model(args.predict, custom_objects={'f1_score': f1_score})
-        ak.predict(texts, 'rnn-output.csv')
+    if args.fit:
+        _, tags, train_sentences = reader.read_data(args.train_data)
+        _, test_sentences = reader.read_test_data(args.test_data)
+        ak = ArtikelKlassfizier()
+        ak.fit(tags, list(train_sentences) + list(test_sentences), args.wordvec)
+
+        with open(args.class_path, 'wb') as class_f:
+            pickle.dump(ak, class_f)
+
+    if args.train:
+        _, tags, train_sentences = reader.read_data(args.train_data)
+        with open(args.class_path, 'rb') as class_f:
+            ak = pickle.load(class_f)
+        ak.train(train_sentences, tags, args.model_path)
+
+    if args.predict:
+        _, texts = reader.read_test_data(args.test_data)
+        with open(args.class_path, 'rb') as class_f:
+            ak = pickle.load(class_f)
+        model = load_model(args.model_path, custom_objects={'f1_score': f1_score})
+        ak.predict(list(texts), 'rnn-output.csv')
 
--- hw5/utils.py
+++ hw5/utils.py
@@ -1,18 +1,9 @@
-import nltk
 import pickle
+import keras.backend as K
 
 class DataReader():
     def __init__(self):
-        self.translator = str.maketrans('', '', '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~')
-        with open('eng-stop-words.pkl', 'rb') as f:
-            stop_words_list = pickle.load(f)
-            self.stop_words = set(stop_words_list)
-
-    def remove_punc(self, sent):
-        return sent.translate(self.translator)
-
-    def remove_stop_words(self, tokens):
-        return [token for token in tokens if token not in self.stop_words]
+        pass
 
     def read_data(self, data_path):
         data = []
@@ -23,10 +14,7 @@ class DataReader():
                 line_id = int(sp_line[0][:-1])
                 tags = sp_line[1].split()
                 text_str = sp_line[2][1:].lower()
-                text_str = self.remove_punc(text_str)
-                tokens = nltk.word_tokenize(text_str)
-                tokens = self.remove_stop_words(tokens)
-                data.append((line_id, tags, tokens))
+                data.append((line_id, tags, text_str))
         return zip(*data)
  
     def read_test_data(self, data_path):
@@ -37,9 +25,47 @@ class DataReader():
                 sp_line = line.strip().split(',', 1)
                 line_id = int(sp_line[0])
                 text_str = sp_line[1].lower()
-                text_str = self.remove_punc(text_str)
-                tokens = nltk.word_tokenize(text_str)
-                tokens = self.remove_stop_words(tokens)
-                data.append((line_id, tokens))
+                data.append((line_id, text_str))
         return zip(*data)
+
+
+def precision(y_true, y_pred, thresh=0.5):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)), axis=1)
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)), axis=1)
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred, thresh=0.5):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)), axis=1)
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)), axis=1)
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def old_f1_score(y_true, y_pred, thresh=0.5):
+    """Compute the F1 score
+
+    Parameters:
+        y_true: pass by fit function
+        y_pred: pass by fit function
+    """
+
+    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+        return 0
+
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    bb = 1
+    f_score = K.mean((1 + bb) * (p * r) / (bb * p + r + K.epsilon()))
+    return f_score
+
+
+def f1_score(y_true, y_pred):
+    thresh = 0.4
+    y_pred = K.cast(K.greater(y_pred, thresh), dtype='float32')
+    tp = K.sum(y_true * y_pred, axis=1)
+    precision = tp / (K.sum(y_pred, axis=1) + K.epsilon())
+    recall = tp / (K.sum(y_true, axis=1) + K.epsilon())
+    return K.mean(2 * ((precision * recall) / (precision + recall + K.epsilon())))
 
